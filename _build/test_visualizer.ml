8e9eb9cfa42b55b049afa42c3f93edaa812fb990
Andrew Wang <azw7@cornell.edu>
2015-12-01 17:18:48 -0500

    axes for scatter

--- _build/test_visualizer.ml
+++ _build/test_visualizer.ml
@@ -2,6 +2,7 @@ open Visualizer
 open Table
 
 let _ =
+  print_endline "Drawing table.";
   let t1 = empty_table "test1" [("Temperature", Int 0);("Day", String "")] in
   
   let n1 = create_node [ref (Int 45); ref (String "Monday")] in
@@ -17,3 +18,21 @@ let _ =
   let _ = insert n5 t1 in
 
   print_tabular t1
+
+let _ =
+  print_endline "Drawing 2d scatter.";
+  let t1 = empty_table "test1" [("Temperature", Int 0);("Day", String "")] in
+  
+  let n1 = create_node [ref (Int 45); ref (Float 20.)] in
+  let n2 = create_node [ref (Int 12); ref (Float 15.)] in
+  let n3 = create_node [ref (Int 6); ref (Float (-3.2))] in
+  let n4 = create_node [ref (Int 17); ref (Float 7.)] in
+  let n5 = create_node [ref (Int (-3)); ref (Float 4.)] in
+
+  let _ = insert n1 t1 in
+  let _ = insert n2 t1 in
+  let _ = insert n3 t1 in
+  let _ = insert n4 t1 in
+  let _ = insert n5 t1 in
+
+  visualize t1 Scatter2d
