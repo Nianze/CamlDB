6ce821150bc03e89e990e6419f16884ed6444f61
Joseph <nl443@cornell.edu>
2015-12-02 22:44:51 -0500

    ast.mli specifications update

--- ast.mli
+++ ast.mli
@@ -11,55 +11,62 @@ type typ = Table.t
 (** the binary operators *)
 type operator = Table.operator
 
-(* ex. ("column1", Eq, 3) *)
+(** ex. ("column1", Eq, 3) *)
 type cond = Table.condition
 
-(* condition tree type *)
+(** condition tree type
+   note that AND has a higher precedence than OR
+ *)
 type c_tree = Table.cond_tree
 
 (** SELECT TOP int (PERCENT) *)
 type top_t = Table.top_t
 
-(* ASC|DESC indicate how to sort the output data *)
+(** ASC|DESC indicate how to sort the output data *)
 type order = Table.order
 
-(* plot method *)
+(** plot method:
+  VisNone means none graph
+  Scatter2d means scatter graph
+  Hist2d means histogram graph
+  BarGraph means bar graph
+  LineGraph means line graph
+ *)
 type plot = Visualizer.vis_method
 
 (** (OCaml) values of type expr represent SQL expressions.
     Here are some examples of how expressions are represented:
      - Int 7 represents 7
      - Bool true represents true
-     - Order: ASC represents Ascending, DESC represents descending order
-     - TbName "GPA" represents table_name "GPA"
      - String "Tom" represents value "Tom"
-     - BinOp (Gt, e1, e2) represents e1 > e2
-     - SelCol (e1, e2, e3) represents SELECT col_name, col_name FROM table_name
-     - SelTop (e1, e2) represents SELECT TOP (11%||2) col_name
-       e2:Top (11,true) represents TOP 11 PERCENT
-          Top (2,false) represents TOP 2
-     - Distin (e1, e2) represents SELECT DISTINCT col_name FROM table_name
-     - Where (e1, e2) represents WHERE col_name true|false
-     - Sort (e1, e2) represents ORDER BY col_name ASC|DESC
-     - Insert (e, [e1;e2;...])
-       represents INSERT INTO table_name VALUES (val1,val2,...)
-     - InsCol (e,[e11;e12;...], [e21;e22;...]) represents
-       INSERT INTO table_name (col1,col2,...) VALUES (val1, val2,...)
-     - Update (e, [e1;e2;...], [e1';e2';...]) represents
-       UPDATE table_name SET col1=val1, col2=val2, ...
-       WHERE condition1, condition2, ...
-     - UpdAll (e, [e1;e2;...]) represents
-       UPDATE table_name SET col1=val1, col2=val2, ...
-     - Delete (e, [e1;e2;...]) represents
-       DELETE FROM table_name WHERE condition1, condition2,...
-     - DelAll (TbName GPA) represents DELETE FROM "GPA"
-     - Create (e,[e1;e2;...]) represents
-       CREATE TABLE table_name
-       (col_name1 type1, col_name2, type2, ...)
-     - Union (e1,e2) represents
-       SELECT * FROM table1
-       UNION ALL
-       SELECT * FROM table2;
+     - TbName "Student" represents table_name Student
+     - ColName "GPA" represents column name "GPA"
+     - Path (tb,col) represents the column [col] in table [tb]
+     - SelCol ([col1;col2], tb, plot) represents SELECT col1,col2 FROM tb (# plot);
+     - SelTop (top, [col1;col2], tb, plot)
+         represents SELECT TOP top col1,col2 FROM tb (#plot);
+         top = TopNum int || TopPercent int
+     - Distin (col, tb, plot) represents SELECT DISTINCT col FROM tb (#plot);
+     - Where (cond_tree, tb) represents WHERE cond_tree FROM tb;
+     - Sort (col, ord, tb)
+         represents FROM tb ORDER BY col ord, where ord = ASC|DESC;
+     - InsRow ([Int 1; String "a"], tb)
+         represents INSERT INTO tb VALUES (1,"a");
+     - InsCol ([col1;col2],[Int 1;Bool true], tb)
+         represents INSERT INTO tb (col1,col2) VALUES (1,true);
+     - UpdAll ([col1=1;col2=Bool false], tb)
+         represents UPDATE tb SET col1=1, col2=false;
+     - Update (cond_tree, [col1=1;col2=true], tb)
+         represents UPDATE tb SET col1=1, col2=true WHERE cond_tree;
+     - DelAll tb represents DELETE FROM tb;
+     - Delete (cond_tree, tb) represents DELETE FROM tb WHERE cond_tree;
+     - Create (tb,[(col1,INT 0);(col2,BOOL false),(col3, String "")])
+         represents CREATE TABLE tb (col1 INT, col2 BOOL, col3 STRING);
+     - Union (e1,e2) where e1= SelCol ([col1;col2], tb1, VisNone),
+         e2= SelCol ([col3], tb2, VisNone),
+         represents SELECT col1,col2 FROM tb1 UNION ALL SELECT col3 FROM tb2;
+     - Joins(tb1, tb2, [tb1.col1,tb2.col3], (tb1.col3,tb2.col4)) represents
+         SELECT tb1.col1,tb2.col3 FROM tb1 JOIN tb2 ON tb1.col3=tb2.col4;
      *)
 type expr =
   | Int      of int
@@ -81,4 +88,4 @@ type expr =
   | Delete   of c_tree * expr
   | Create   of expr * (expr * typ) list
   | Union    of expr * expr
-  | Joins    of expr * expr * expr list * (expr * expr) (* (Tb1, Tb2, path list, (path1,path2)) *)
+  | Joins    of expr * expr * expr list * (expr * expr)
