b835c86a0af962e5260ad209fade3a89e67a1723
Andrew Wang <azw7@cornell.edu>
2015-12-05 12:50:36 -0500

    fix glitch in repl

---
+++ _build/data_processor.ml
@@ -0,0 +1,384 @@
+open Table
+
+
+
+(*
+SQL:
+CREATE TABLE table_name
+(s
+column_name1 data_type(size),
+column_name2 data_type(size),
+column_name3 data_type(size),
+....
+);
+
+create a table with name [table_name], specify the type and
+column name of each column by [col_name_list]
+*)
+let create_table (table_name: string) (col_name_list: (colname * t) list)
+: status * table = empty_table table_name col_name_list
+
+
+(*
+SQL:
+INSERT INTO table_name
+VALUES (value1,value2,value3,...);
+
+insert a new row with values [val_list] in the order of columns
+into table [t] and return a subtable
+*)
+let insert_values (val_list: t list) (t: table) : status =
+        let row = create_node (List.map (fun x -> ref x) val_list) in
+        insert row t
+
+(*
+SQL:
+INSERT INTO table_name (column1,column2,column3,...)
+VALUES (value1,value2,value3,...);
+
+insert a new row, both the column names and the values to be
+inserted [val_list] are speicified in [col_list]
+*)
+let insert_col_values (col_list : (colname * t) list)
+(t:table) : status =
+  let colnames = List.map (fun (x, y) -> (x, ref y)) (get_colnames t) in
+  List.(iter
+    (fun (x, y) -> if mem_assoc x col_list then y:=assoc x col_list) colnames
+  );
+  let col = List.map (fun (x,y) -> y) colnames in
+  let row = create_node col in
+  insert row t
+
+let update_node (n: node) (colnames: (colname * t) list)
+(pair_list : (colname * t) list): unit =
+        List.(iter2
+                (fun (c, ts) t ->
+                if mem_assoc c pair_list
+                then (t := (assoc c pair_list))
+                else () )
+        colnames n.value)
+
+(*
+SQL:
+SELECT column_name, column_name FROM table_name;
+
+select some particular columns in a list of colunms [col_list]
+from table [t] and return a subtable
+*)
+let select_col (col_list :colname list) (t: table): status * table =
+  let colnames = get_colnames t in
+  let out_cols = List.filter (fun (x,_)-> List.mem x col_list) colnames in
+  let (_, out_tb) = empty_table (get_tablename t) out_cols in
+  let out_index = List.map (get_col_i t) col_list in
+  let get_vals val_list = List.map (List.nth val_list) out_index in
+  let out node =
+    let pairs = List.combine (col_list) (List.map (!) (get_vals node.value)) in
+    insert_col_values pairs out_tb in
+  let rec helper orig_n stat =
+    match stat with
+      | DBError e -> (DBError e, out_tb)
+      | Success -> match orig_n with
+        | Some node -> let next = node.next in
+          helper next (out node)
+        | None -> (Success, out_tb) in
+   helper t.first Success
+
+(*
+SELECT TOP number|percent column_name(s)
+FROM table_name;
+
+select some particular columns in a list of colunms [col_list]
+from table [t] and return a subtable
+*)
+let select_top (top:top_t) (col_list:colname list) (t: table): status * table =
+  let colnames = get_colnames t in
+  let out_cols = List.filter (fun (x,_)-> List.mem x col_list) colnames in
+  let (_, out_tb) = empty_table (get_tablename t) out_cols in
+  let out_index = List.map (get_col_i t) col_list in
+  let get_vals val_list =
+    List.map (fun n -> !(List.nth val_list n)) out_index in
+  let out node =
+    let pairs = List.combine (col_list) (get_vals node.value) in
+    insert_col_values pairs out_tb in
+  let rec helper orig_n stat count_down =
+    if count_down = 0 then (Success, out_tb) else
+      match stat with
+        | DBError e -> (DBError e, out_tb)
+        | Success -> match orig_n with
+          | Some node -> let next = node.next in
+            helper next (out node) (count_down-1)
+          | None -> (Success, out_tb) in
+  match top with
+    | TopNum num -> helper t.first Success num
+    | TopPercent p -> if p>100 || p<0
+      then (DBError "Percentage out of range", out_tb)
+      else helper t.first Success (t.numcol*p/100)
+
+(*
+SQL:
+SELECT DISTINCT column_name FROM table_name;
+
+get all the distinct values of a column with the name of [col_name]
+from table t and return a subtable
+*)
+let distinct (col_name :colname) (t :table) : status * table =
+  if not (col_in_table t col_name)
+    then (DBError "column names not found.",
+      snd (empty_table (get_tablename t) [])
+    )
+    else
+      let colnames = get_colnames t in
+      let out_cols = List.find (fun (x,_)-> x = col_name) colnames in
+      let (_, out_tb) = empty_table (get_tablename t) [out_cols] in
+      let out_index = get_col_i t col_name in
+      let get_vals val_list = !(List.nth val_list out_index) in
+      let out node =
+        let pairs = (col_name,get_vals node.value) in
+        insert_col_values [pairs] out_tb in
+      let rec helper orig_n stat val_buffer =
+        match stat with
+          | DBError e -> (DBError e, out_tb)
+          | Success -> match orig_n with
+            | Some node -> let next = node.next in
+              let node_val = get_vals node.value in
+              if List.mem node_val val_buffer
+                then helper next Success val_buffer
+                else helper next (out node) (node_val::val_buffer)
+            | None -> (Success, out_tb) in
+       helper t.first Success []
+
+
+(*
+SQL:
+SELECT *
+FROM table_name
+WHERE column_name operator value;
+
+filter the table [t] according to the conditions in [cond_list]
+and return a subtable
+*)
+let where (cond_list: cond_tree) (t :table) :status * table =
+  match find cond_list t with
+  | (nl, Success) -> (
+  	let (_, new_t) = create_table (get_tablename t) (get_colnames t) in
+  	List.iter (fun n -> ignore (insert (create_node n.value) new_t)) nl;
+  	(Success,new_t) )
+  | (_, DBError e) -> (DBError e, snd (empty_table "" []) )
+
+
+
+let get_cmp (o: order) (i: int) =
+  match o with
+  | ASC -> (
+    fun n1 n2 ->
+      let v1 = List.nth n1 i in
+      let v2 = List.nth n2 i in
+      if v1 > v2 then 1
+      else if v1 = v2 then 0
+      else -1)
+  | DESC -> (
+    fun n1 n2 ->
+      let v1 = List.nth n1 i in
+      let v2 = List.nth n2 i in
+      if v1 < v2 then 1
+      else if v1 = v2 then 0
+      else -1)
+
+(*
+SQL:
+SELECT *
+FROM table_name
+ORDER BY column_name ASC|DESC;
+
+sort the table [t] in the ascending or descending order of [o]
+of column [col_name] and return a subtable
+*)
+let sort (col_name: colname) (o: order) (t: table) : status * table =
+        let colnames = get_colnames t in
+        if (List.mem_assoc col_name colnames) = false then
+                (DBError "sort: col_name not found", t)
+        else
+                let l_node = table_to_list t in
+                let l = List.map (fun n -> n.value) l_node in
+                let index = ref 0 in
+                List.iteri
+                (fun i (c, _) -> if c = col_name then index:= i else ())
+                colnames;
+                let sorted = List.sort (get_cmp o !index) l in
+                let node_list = List.map (fun v -> create_node v) sorted in
+                list_to_table (get_tablename t) (get_colnames t) node_list
+
+
+
+
+
+(* check if each columns in pair_list can be found in colnames
+         return:
+                empty string "" if all columns found,
+                string of names of the columns unfound otherwise
+ *)
+let colname_check (pair_list : (colname * t) list)
+(colnames : (colname * t) list): string =
+	List.(fold_left
+		(fun s (c, _) ->
+			if mem_assoc c colnames then s
+			else if s = "" then c
+			else s ^ ", " ^ c )
+	"" pair_list)
+
+(* check if each columns in pair_list has the same type as originally
+         defined by table in colnames
+         return:
+                empty string "" if all column types match,
+                string of names of the columns that don't type match
+ *)
+let type_check (pair_list : (colname * t) list)
+(colnames : (colname * t) list): string =
+	List.(fold_left
+		(fun s (c, t) ->
+			if match_type t (assoc c colnames) then s
+			else if s = "" then c
+			else s ^ ", " ^ c)
+	"" pair_list)
+
+(*
+SQL:
+UPDATE table_name
+SET column1=value1,column2=value2,...;
+
+update all the rows in the table [t] according to the column
+and value specified by [pair_list]
+*)
+let update_all (pair_list : (colname * t) list) (t:table) : status =
+  let colnames = get_colnames t in
+  let e_find = colname_check pair_list colnames in
+  let e_type = type_check pair_list colnames in
+  match (e_find, e_type) with
+    | ("", "") -> iter (fun n -> update_node n colnames pair_list) t; Success
+    | ("", _) ->
+            DBError ("update_all: type of column "^e_type^" cannot be modified")
+    | _ -> DBError ("update_all: can't find columns: " ^ e_find)
+
+(*
+SQL:
+UPDATE Customers
+SET ContactName='Alfred Schmidt', City='Hamburg'
+WHERE CustomerName='Alfreds Futterkiste';
+
+update all the rows that satisfy the conditions in [cond_list]
+in the table [t] according to the column and value specified
+by [pair_list]
+*)
+let update_where (cond_list: cond_tree) (pair_list :(colname * t) list)
+(t:table) : status =
+  match find cond_list t with
+  | (nl, Success) -> (
+    let colnames = get_colnames t in
+    let e_find = colname_check pair_list colnames in
+    let e_type = type_check pair_list colnames in
+    match (e_find, e_type) with
+      | ("", "") ->
+              List.iter (fun n -> update_node n colnames pair_list) nl; Success
+      | ("", _) ->
+        DBError ("update_all: type of column "^e_type^" cannot be modified")
+      | _ -> DBError ("update_all: can't find columns: " ^ e_find)
+    )
+  | (_, DBError e) -> DBError e
+
+(*
+SQL:
+DELETE FROM table_name;
+
+delete all rows in the table [t]
+*)
+let delete_all (t:table) : status =
+  fold_left
+  (fun a n ->
+    match a with
+      | Success -> delete n t
+      | DBError e -> a
+  ) Success t
+
+(*
+SQL:
+DELETE FROM table_name
+WHERE some_column=some_value;
+
+delete all rows in the table [t] that satisfies the conditions in
+[cond_list], disable the row under the hood
+*)
+let delete_where (cond_list: cond_tree) (t:table) : status =
+	match find cond_list t with
+	| (nl, Success) -> List.iter (fun n -> ignore (delete n t)) nl; Success
+	| (_, DBError e) -> DBError e
+
+
+
+(*
+SQL:
+SELECT * FROM table1
+UNION ALL
+SELECT * FROM table2;
+
+concatenate two tables [t1] and [t2] given [t1] and [t2] the same
+number of columns. Assign the concatenated table column names
+specified in [col_name_list]
+*)
+let union_rows (t1: table) (t2: table) (col_name_list: colname list)
+: status * table =
+        (* check colnames in both table, throw error if not exist *)
+        let (s1, t1_cols) = select_col col_name_list t1 in
+        let (s2, t2_cols) = select_col col_name_list t2 in
+        match (s1, s2) with
+                | (DBError e, _) | (_, DBError e) -> (DBError e, t1)
+                | (Success, Success) ->
+                                iter (fun x -> ignore (insert x t1)) t2;
+                                (Success, t1)
+
+let split_name n =
+  let start = (String.index n '#') + 1 in
+  String.sub n start ((String.length n) - start)
+
+let inner_join t1 t2 pathnames (path1, path2) =
+  let (c1, c2) =
+    if fst path1 = get_tablename t1 then (snd path1, snd path2)
+    else (snd path2, snd path1) in
+
+  let make_colnames t =
+    List.map (fun (x, typ) ->
+      (get_tablename t) ^ "#" ^ x, typ) (get_colnames t) in
+
+  let (sts, t) = create_table "join" (make_colnames t1 @ (make_colnames t2)) in
+  match sts with
+  | DBError e -> (sts, t)
+  | Success -> (
+    let c1_index = snd (List.fold_left
+  			(fun (i, found) v ->
+  			  (i + 1, if fst v = c1 then i else found))
+  			    (0, -1) (get_colnames t1)) in
+
+    if c1_index >= 0 then
+      let status = ref Success in
+      iter (fun t1elt ->
+        let c1_entry = !(List.nth t1elt.value c1_index) in
+        match where (Cond (c2, EQ, c1_entry)) t2 with
+        | (Success, matches) ->
+  	 iter (fun mtch ->
+  	   let node =
+  	     {prev = None; next = None; value = t1elt.value @ mtch.value} in
+  	   (match insert node t with
+  	   | Success -> ()
+  	   | DBError _ -> status := DBError "Could not join tables"
+  	   )
+  	 ) matches
+        | (DBError _, _) -> status := DBError "Could not join tables"
+      ) t1;
+      let joined_colnames = List.map (fun (t, c) -> t ^ "#" ^ c) pathnames in
+      match select_col joined_colnames t with
+      | (Success, t') ->
+         let cn = List.map (fun (n, typ) -> (split_name n, typ)) t'.colnames in
+         let (_, t'') = list_to_table "join" cn (table_to_list t') in
+         (!status, t'')
+      | (DBError _, sel_t) -> (!status, sel_t)
+    else (DBError "Join: column name not found", snd  (empty_table "" []) )
+  )
---
+++ _build/table.ml
@@ -0,0 +1,447 @@
+(******************* Basic data type *******************)
+
+(* A [t] stores the type and values of each value *)
+type t =
+	| Int of int
+	| String of string
+	| Float of float
+	| Bool of bool
+
+(* [match_type t1 t2] returns true if the type of t1 t2 is the same
+ * false otherwise
+ *)
+let match_type (t1:t) (t2:t) : bool =
+	match (t1, t2) with
+	| (Int _, Int _) | (Float _, Float _) | (String _, String _)
+	| (Bool _, Bool _) -> true
+	| _ -> false
+
+(* [type_string v] convert a [v] to a string of its type
+ *)
+let type_string (v:t) : string =
+	match v with
+	| Int _ -> "int"
+	| String _ -> "string"
+	| Bool _ -> "bool"
+	| Float _ -> "float"
+
+type order = DESC | ASC
+type top_t = TopNum of int | TopPercent of int
+
+(******************* Status of operations *******************)
+
+(* [status] is the status of operations, it includes all the erros *)
+
+type status =
+	| Success
+	| DBError of string
+
+
+(******************* Node *******************)
+
+(* A [node] is a node of a mutable doubly-linked list.
+ * It stores a row in the table.
+ * It contains a value of type [val] and optionally has
+ * pointers to previous and/or next nodes. *)
+type node = {
+	mutable prev : node option;
+	mutable next : node option;
+	mutable value : (t ref) list
+}
+
+(* [create_node v] is a node containing value [v] with
+ * no links to other nodes. *)
+let create_node v  = {prev=None; next=None; value=v}
+
+
+
+(******************* Compare *******************)
+(* Operators for comparing values
+EQ: =	Equal
+NE: <>	Not equal
+GT: >	Greater than
+LT: <	Less than
+GE: >=	Greater than or equal
+LE: <=	Less than or equal
+*)
+type operator = EQ | NE | GT | LT | GE | LE
+
+(* [compare_value v1 v2] compare two primitive types *)
+let compare_value (op: operator) (v1: 'a) (v2: 'a) : bool =
+	match op with
+	| EQ -> v1 = v2
+	| NE -> v1 <> v2
+	| GT -> v1 > v2
+	| LT -> v1 < v2
+	| GE -> v1 >= v2
+	| LE -> v1 <= v2
+
+(* [compare_value t1 t2] compare two data of type [t] *)
+let compare (op: operator) (v1:t) (v2:t) : bool * status =
+	match (v1, v2) with
+	| (Int i1, Int i2) -> (compare_value op i1 i2, Success)
+	| (Float f1, Float f2) -> (compare_value op f1 f2, Success)
+	| (String s1, String s2) -> (compare_value op s1 s2, Success)
+	| (Bool b1, Bool b2) -> (compare_value op b1 b2, Success)
+	| _ -> false, DBError "compare: compare type mismatch"
+
+
+(******************* Table type *******************)
+type colname = string
+
+(* An [table] is a table.
+ * [name] is the table name
+ * [colnames] c a list of column names and column types tuple
+ * [numcol] is the number of columns in the table
+ * [numrow] is the number of rows in the table
+ * The table content is stored in a doubly linked list
+ * Each row is stored in a 'a node.
+ *)
+type table = {
+	name: string;
+	colnames: (colname * t) list;
+	numcol : int;
+	mutable numrow : int;
+	mutable first : node option;
+	mutable last : node option;
+}
+
+
+(******************* Condition *******************)
+(* ex. ("column1", Eq, 3) *)
+type condition = colname * operator * t
+
+type cond_tree =
+  | Cond of condition
+  | And of cond_tree * cond_tree
+  | Or of cond_tree * cond_tree
+
+(******************* Table Accessor *******************)
+
+(* [get_tablename t] gets table name of table [t] *)
+let get_tablename (t:table) : string = t.name
+
+(* [get_colnames t] gets a list of column name of table [t] *)
+let get_colnames (t:table) : (colname * t) list = t.colnames
+
+(* [col_in_table t col_name] checks if col [col_name] is in table [t]  *)
+let col_in_table (t: table) (col_name: colname): bool =
+	let colnames = t.colnames in
+	List.mem_assoc col_name colnames
+
+(* [get_col_i t col_name] gets the index of col [col_name]
+ * return -1 if col [col_name] not in table [t]
+ *)
+let get_col_i (t: table) (col_name: colname): int =
+	let colnames = t.colnames in
+	let index = ref (-1) in
+	List.iteri
+	(fun i (c, _) -> if col_name = c then index := i else ())
+	colnames;
+	!index
+
+(* [get_first t] gets the first node in table [t] *)
+let get_first (t:table) : node option = t.first
+
+(* [get_types t] gets the last node in table [t] *)
+let get_last (t:table) : node option = t.last
+
+
+(******************* Table Helper *******************)
+let in_some (a: 'a option) : 'a =
+	match a with
+	| None -> failwith "in_some"
+	| Some b -> b
+
+(* [empty_table name colnames coltypes] is an empty table. *)
+let empty_table (name :string) (colnames: (colname * t) list): status *table =
+	let (names, _) = List.split colnames in
+	let no_duplicate_colnames names =
+		let (_, has_dup) =  List.(fold_left
+			(fun (l, b) n ->
+			if mem n l then (n::l, false)
+			else (n::l, b) )
+			([], true) names)
+		in has_dup
+	in if no_duplicate_colnames names = true then
+		(Success, {
+			name = name;
+			colnames = colnames;
+			numcol = List.length colnames;
+			numrow = 0;
+			first = None;
+			last = None
+		})
+	else
+		(DBError "create_table: duplicate column names", {
+			name = "";
+			colnames = [];
+			numcol = 0;
+			numrow = 0;
+			first = None;
+			last = None
+		})
+
+(* check if each columns in pair_list has the same type as originally
+	 defined by table in colnames
+	 return:
+	 	empty string "" if all column types match,
+		string of names of the columns that don't type match
+ *)
+let insert_type_check (d_list : (t ref) list)
+(colnames : (colname * t) list): bool =
+	List.fold_left2
+	(fun a tref (_, d) ->
+		a && (match_type (!tref) d)
+	) true d_list colnames
+
+(* [insert r t] inserts a row [r] to the top of a
+ * table [t].
+ * require: nothing about the default prev, next of [r]
+ *)
+let insert (r: node) (t:table) : status =
+	if insert_type_check r.value t.colnames then
+		match t.first with
+		| None ->
+			r.prev <- None;
+			r.next <- None;
+			t.first <- Some r;
+			t.last <- Some r;
+			t.numrow <- 1;
+			Success
+		| Some n ->
+			r.prev <- None;
+			r.next <- Some n;
+			t.first <- Some r;
+			n.prev <- Some r;
+			t.numrow <- (t.numrow + 1);
+			Success
+	else DBError "insert: row to insert doesn't type match with table"
+
+(* [delete r t] deletes a row [r] from table [t]. *)
+let delete (r: node) (t:table) : status =
+	match (r.prev, r.next) with
+	| (None, None) ->
+		(t.first <- None);
+		(t.last <- None);
+		(r.prev <- None);
+		(r.next <- None);
+		t.numrow <- 0;
+		Success
+	| (Some x, None) ->
+		(x.next <- None);
+		(t.last <- Some x);
+		(r.prev <- None);
+		(r.next <- None);
+		t.numrow <- t.numrow - 1;
+		Success
+	| (None, Some y) ->
+		(y.prev <- None);
+		(t.first <- Some y);
+		(r.prev <- None);
+		(r.next <- None);
+		t.numrow <- t.numrow - 1;
+		Success
+	| (Some x, Some y) ->
+		(x.next <- Some y);
+		(y.prev <- Some x);
+		(r.prev <- None);
+		(r.next <- None);
+		t.numrow <- t.numrow - 1;
+		Success
+
+(* [cond_row cond_list r] checks if row [n] satisfies condions in
+ * [cond_list] and returns true or false and the status
+ * requires: [n] to be non-empty
+ *)
+let rec cond_row (cond_list: cond_tree) (colnames: (colname * t) list)
+(n: node) : bool * status =
+	match cond_list with
+	| Cond (col, op, v) -> (
+		let num_found = List.(length (filter (fun (n,t) -> n = col) colnames)) in
+
+		if num_found < 1
+		then (false, DBError ("Column name " ^ col ^ " is not found.") )
+		else if num_found > 1
+		then (false, DBError ("Duplicate columns " ^ col ^ " found.") )
+		else if List.(length colnames <> length n.value)
+		then (false, DBError
+		"cond_row: Number of columns mismatch between colname and row content.")
+		else if
+		List.(length
+			(filter (fun (n,t) -> (n = col) && (match_type t v)) colnames)
+		) <> 1
+		then (false, DBError ("Column " ^ col ^ " has the wrong type") )
+
+		else
+		let pair_list = List.combine colnames n.value in
+		let (_, content) = List.find (fun ((cn, _), _) -> cn = col) pair_list in
+		compare op !content v
+		)
+	| And (c1, c2) -> (
+		match (cond_row c1 colnames n, cond_row c2 colnames n) with
+			| ((_, DBError e), _) -> (false, DBError e)
+			| (_, (_, DBError e)) -> (false, DBError e)
+			| ((true, _), (true, _)) -> (true, Success)
+			| _ -> (false, Success)
+		)
+	| Or (c1, c2) -> (
+		match (cond_row c1 colnames n, cond_row c2 colnames n) with
+			| ((_, DBError e), _) -> (false, DBError e)
+			| (_, (_, DBError e)) -> (false, DBError e)
+			| ((false, _), (false, _))  -> (false, Success)
+			| _ -> (true, Success)
+		)
+
+
+(* [find cond_list t] finds rows satisfies condions in
+ * [cond_list] in table [t].
+ *)
+let find (cond_list: cond_tree) (t: table): (node list) * status =
+	let rec helper (n : node option) (l: node list): (node list) * status =
+		match n with
+		| None -> (l,Success)
+		| Some r ->
+			match cond_row cond_list t.colnames r with
+			| (true, Success) ->
+				(* move to front *)
+				helper r.next (r::l)
+			| (false, Success) -> helper r.next l
+		| (_, error) -> ([], error)
+	in
+	let (l, s) = helper t.first [] in
+	List.iter (fun n -> ignore (delete n t); ignore (insert n t)) l;
+	(l, s)
+
+
+(* [list_to_table node_list] makes a copy of each node in [node_list]
+ * and insert them into a new table and return that table
+ *)
+let list_to_table (node_list: node list) (table_name: string)
+(colnames: (colname * t) list) : status * table =
+	let (s, t) = empty_table table_name colnames in
+
+	let rec helper = function
+	| [] -> Success
+	| hd::tl ->
+		let r = create_node hd.value in
+		(match insert r t with
+			| Success -> helper tl
+			| DBError e -> DBError e ) in
+
+	match s with
+		| DBError e -> (DBError e, t)
+		| Success -> (
+		match helper node_list with
+			| Success -> (Success, t)
+			| DBError e -> (DBError e, t)
+		)
+
+
+(* [delete_list n_list t] deletes a list of rows [n_list] from table [t]. *)
+let rec delete_list (n_list: node list) (t:table) : status =
+	match n_list with
+		| [] -> Success
+		| hd::tl -> (
+			match delete hd t with
+				| Success -> delete_list tl t
+				| DBError e -> DBError e
+			)
+
+(* [delete_find cond_list t] delete all the rows that satisfy [cond_list]
+ * in table [t]
+ *)
+let delete_find (cond_list: cond_tree) (t: table): status =
+	let (node_list, s) = find cond_list t in
+	match s with
+		| Success -> delete_list node_list t
+		| x -> x
+
+(* [iter f t] iterate the table [t] and apply [f] on every row
+ *)
+let iter (f: node -> unit) (t:table): unit =
+	let rec helper f node_o : unit =
+		match node_o with
+			| Some node ->
+				let nxt = node.next in
+				(f node); helper f nxt
+			| None -> ()
+	in helper f t.first
+
+(* [fold_left f a t] iterate through table [t], apply [f] on every row
+ * return: [a] accumulator
+ *)
+let fold_left (f: 'a -> node -> 'a) (a: 'a) (t:table): 'a =
+	let rec helper f a node_o=
+		match node_o with
+			| Some node ->
+				let nxt = node.next in
+				helper f (f a node) nxt
+			| None -> a
+	in helper f a t.first
+
+(* [table_to_list t] iterate the table [t] return a list of nodes
+ * the most recent node is the first
+ *)
+let table_to_list (t: table): (node list) =
+	let rec helper l node_o  =
+		match node_o with
+			| Some node -> helper (node::l) (node.prev)
+			| None -> l
+	in helper [] t.last
+
+(* [list_to_table names colnames rows] converts a list to a table.
+ * regard the first row in list as the most recent one
+ * [name] - table name
+ * [colnames] - colnames
+ * [rows] - a list of node
+ *)
+let list_to_table (name :string) (colnames: (colname * t) list)
+	(rows: node list) : status * table =
+	let (s, t) = empty_table name colnames in
+	match s with
+		| Success ->
+			(List.fold_right
+				(fun n a ->
+					match a with
+					 	| Success -> insert n t
+					  | DBError e -> a
+				) rows Success,
+			t)
+		| DBError e -> (DBError e, t)
+
+
+(* [get_col_i t col_name] gets the col [col_name] as a t list *)
+let get_col (t: table) (col_name: colname): t list =
+	let index = get_col_i t col_name in
+	if index = -1 then []
+	else
+	fold_left (fun a n -> a @ [!(List.nth (n.value) index)]) [] t
+
+
+(* [node_equal n1 n2] compare two nodes and return true if they are equal
+ * structrually
+ *)
+let node_equal (n1: node) (n2 :node) : bool =
+  List.fold_left2
+  (fun a v1 v2-> if !v1 = !v2 then a else false)
+  true n1.value n2.value
+
+(* [node_list_equal node lst] compare the value of a node with a list and
+ * return true if they are equal structrually
+ *)
+let node_list_equal node lst =
+  let nl = List.map (fun x -> !x) node.value in
+  nl = lst
+
+(* [table_equal t1 t2] returns true if each node in two table equals
+ * structurally
+ *)
+let table_equal t1 t2 =
+  if t1.numrow = t2.numrow && t1.numcol = t2.numcol then
+    let l1 = table_to_list t1 in
+    let l2 = table_to_list t2 in
+    List.fold_left2
+    (fun a n1 n2 -> if node_equal n1 n2 then a else false) true l1 l2
+  else false
+
