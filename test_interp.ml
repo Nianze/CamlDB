4a4ac5cefe9a3d5dcfcc1a7fe69c3032b1c0fc25
Yuyan-Lydia-Tang <lydia.yuyantang@gmail.com>
2015-12-05 21:07:05 -0500

    clean up the files

--- test_interp.ml
+++ test_interp.ml
@@ -7,7 +7,7 @@ let exec s =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
   eval ast
-    
+
 let _ =
   let t = empty_table "t" [("a", Int 0); ("b", String "")] in
   assert (table_equal (exec "CREATE TABLE t (a INT, b STRING);") t);
@@ -32,7 +32,8 @@ let _ =
   ignore (exec "INSERT INTO v VALUES (3, 4);");
   ignore (insert_values [Int 3; Int 5] v);
   ignore (exec "INSERT INTO v VALUES (3, 5);");
-  let join = snd (inner_join t v [("t", "a"); ("v", "c")] (("t", "a"), ("v", "a"))) in
+  let join =
+  snd (inner_join t v [("t", "a"); ("v", "c")] (("t", "a"), ("v", "a"))) in
   assert (table_equal (exec "SELECT t.a, v.c FROM t JOIN v ON t.a=v.a;") join);
   ignore (delete_where (Cond ("a", EQ, Int 3)) t);
   assert (table_equal (exec "DELETE FROM t WHERE a=3;") t)
